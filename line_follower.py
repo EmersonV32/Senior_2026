16d908ce3ee4596d91b367a60663f45b8de7fe7b
Pandaboiv2 <bievan0427@gmail.com>
2026-04-10 15:18:51 -0400

    Add files via upload

---
+++ line_follower.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, ColorSensor)
+from pybricks.parameters import Port, Stop, Color
+from pybricks.tools import wait, StopWatch, DataLog
+m2 = Motor(Port.B)
+m3 = Motor(Port.C)
+csR = ColorSensor(Port.S1)
+csL = ColorSensor(Port.S4)
+
+def pid_line_follower(follow_sensor, stop_sensor,
+                      motor_right, motor_left, base_speed,
+                      Kp, Kd,
+                      Ki = 0,
+                      target=48.5,
+                      max_angle=None,
+                      stop_mode="c",
+                      stop_threshold=20,
+                      side="l"): 
+    integral = 0
+    last_error = 0
+
+    motor_right.reset_angle(0)
+    motor_left.reset_angle(0)
+
+    while True:
+        reflection = follow_sensor.reflection()
+        stop_reflection = stop_sensor.reflection()
+
+        # Stop condition
+        if stop_mode == "c":
+            if stop_reflection < stop_threshold:
+                break
+        elif stop_mode == "a":
+            if max_angle is not None:
+                if abs(motor_right.angle()) >= max_angle or abs(motor_left.angle()) >= max_angle:
+                    break
+
+        # Calculate error based on side
+        if side == "l":
+            error = reflection - target
+        elif side == "r":
+            error = target - reflection
+        else:
+            error = target - reflection
+
+        integral += error
+        derivative = error - last_error
+        turn = Kp * error + Ki * integral + Kd * derivative
+
+        right_speed = base_speed + turn
+        left_speed = base_speed - turn
+
+        motor_right.run(-right_speed)
+        motor_left.run(left_speed)
+
+        last_error = error
+        wait(10)
+
+    motor_right.stop(Stop.BRAKE)
+    motor_left.stop(Stop.BRAKE)
+
+pid_line_follower(csL, csR, m2, m3, base_speed=500, Kp=2, Kd=3, side="r", stop_mode="a", max_angle=2000)
