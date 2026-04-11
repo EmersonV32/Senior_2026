c279d5aa026f58f91c855f73fbb9960c45d66e75
pandaboiv2 <bievan0427@gmail.com>
2026-04-10 20:11:22 -0400

    .

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ motor_d.reset_angle(0)
 wait(50)
 
 # ===== BASIC MOVEMENT FUNCTION =====
-def move_motors(left_speed, right_speed, duration_ms=None, rotations=None):
+def move_motors(left_speed, right_speed, duration_ms=None, rotations=None, degrees=None):
     if duration_ms is not None:
         left_motor.run_time(left_speed, duration_ms, Stop.BRAKE, wait=False)
         right_motor.run_time(right_speed, duration_ms, Stop.BRAKE, wait=True)
@@ -29,6 +29,10 @@ def move_motors(left_speed, right_speed, duration_ms=None, rotations=None):
         angle = 360 * rotations
         left_motor.run_angle(left_speed, angle, Stop.BRAKE, wait=False)
         right_motor.run_angle(right_speed, angle, Stop.BRAKE, wait=True)
+    
+    elif degrees is not None:
+        left_motor.run_angle(left_speed, degrees, Stop.BRAKE, wait=False)
+        right_motor.run_angle(right_speed, degrees, Stop.BRAKE, wait=True)
 
 
 # ===== MAIN PROGRAM =====
@@ -37,7 +41,7 @@ motor_a.run_angle(-1000, 280) # moves the white thing up
 move_motors(500, -500, duration_ms=1000) # aligns with the wall
 wait(500)
 
-move_motors(-300, 300, rotations=1.23) # moves to the first row of yellow blocks
+move_motors(-300, 302, rotations=1.23) # moves to the first row of yellow blocks
 left_motor.stop(Stop.BRAKE)
 right_motor.stop(Stop.BRAKE)
 wait(500)
@@ -48,26 +52,33 @@ wait(500)
 motor_d.run(500) # holds the blocks in place
 wait(1000)
 
-motor_a.run_angle(-1000, 350) # move up the white thing
+motor_a.run_time(-1000, 1100)  # move up the white thing
 
-move_motors(-200, 200, rotations=0.4) # moves to the second row of yellow blocks
+move_motors(-200, 210, rotations=0.3613548645132) # moves to the second row of yellow blocks
 left_motor.stop(Stop.COAST)
 right_motor.stop(Stop.COAST)
 
-motor_a.run_angle(200, 90) # moves the white thing down a little bit
-wait(2000)
+motor_a.run_angle(200, 165) # moves the white thing down a little bit
+wait(170)
 
 motor_d.stop()
 wait(50)
-motor_d.run_angle(-300, 150) # releases the yellow block on top of the blue block
+motor_d.run_time(-100, 1000) # releases the yellow block on top of the blue block
+
 wait(1000)
 
-move_motors(-100, 100, rotations=0.03)
+motor_a.run_time(350, 1200, wait=False) # moves the white thing down
+wait(100)
 
-motor_a.run_angle(600, 250)
-wait(1000)
+counter = 0
+while counter <= 4:
+    move_motors(-200, -200, degrees=15)
+    move_motors(200, 200, degrees=15)
+    counter +=1
+left_motor.stop(Stop.COAST)
+right_motor.stop(Stop.COAST)
 
-motor_d.run(500)
+motor_d.run(500) # holds the block 
 wait(2000)
 
 # Move forward
@@ -90,52 +101,3 @@ move_until_reflect_condition(
 # Small adjust
 move_motors(-200, 200, rotations=0.5)
 move_motors(500, 500, rotations=0.7)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ===== PID LINE FOLLOW =====
-pid_line_follower(
-    follow_sensor_port=Port.S1,
-    stop_sensor_port=Port.S4,
-    base_speed=400,
-    Kp=2,
-    Kd=3,
-    target=30,
-    stop_mode="c",
-    stop_threshold=20,
-    side="r"
-)
-
-# Adjust
-move_motors(-200, 200, rotations=0.2)
-move_motors(-500, -500, rotations=0.8)
-
-# ===== FINAL PID LINE FOLLOW =====
-pid_line_follower(
-    follow_sensor_port=Port.S1,
-    stop_sensor_port=Port.S4,
-    base_speed=400,
-    Kp=2,
-    Kd=3,
-    target=30,
-    stop_mode="c",
-    stop_threshold=20,
-    side="r"
-)
