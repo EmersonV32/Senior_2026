e6a531d3fcf19940694e827a5ada8358c49ad31c
pandaboiv2 <bievan0427@gmail.com>
2026-04-12 23:00:50 -0400

    lots of changes

--- line_follower.py
+++ line_follower.py
@@ -1,38 +1,12 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor,ColorSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
-# Create your objects here.
-ev3 = EV3Brick()
-# Write your program here.
-ev3.speaker.beep()
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port, Stop
 from pybricks.tools import wait
 
-# Initialize motors (adjust ports if needed)
+ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-
-# Initialize only Motor A
-motor_a = Motor(Port.A)
-motor_b = Motor(Port.B)
-motor_c = Motor(Port.C)
-motor_d = Motor(Port.D)
-motor_a.reset_angle(0)
-motor_b.reset_angle(0)
-motor_c.reset_angle(0)
-motor_d.reset_angle(0)
-
-sensor_a = ColorSensor(Port.S1)
-sensor_b = ColorSensor(Port.S2)
-sensor_c = ColorSensor(Port.S3)
-sensor_d = ColorSensor(Port.S4)
-
 
 def pid_line_follower(follow_sensor_port=Port.S1,
                       stop_sensor_port=Port.S4,
@@ -69,9 +43,9 @@ def pid_line_follower(follow_sensor_port=Port.S1,
 
         # ===== ERROR =====
         if side == "l":
-            error = reflection - target
+            error = target - reflection 
         else:
-            error = target - reflection
+            error = reflection - target
 
         integral += error
         derivative = error - last_error
@@ -92,6 +66,7 @@ def pid_line_follower(follow_sensor_port=Port.S1,
     right_motor.stop(Stop.BRAKE)
     ev3.speaker.beep()  
 
+'''
 pid_line_follower(follow_sensor_port=Port.S4,
                       stop_sensor_port=Port.S1,
                       base_speed=400,
@@ -101,3 +76,4 @@ pid_line_follower(follow_sensor_port=Port.S4,
                       stop_mode="c",
                       stop_threshold=20,
                       side="r")
+'''
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port, Stop
 from pybricks.tools import wait, StopWatch
+from line_follower import pid_line_follower
 
 ev3 = EV3Brick()
 
@@ -10,6 +11,7 @@ left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 motor_a = Motor(Port.A)
 motor_d = Motor(Port.D)
+csL = ColorSensor(Port.S1)
 
 #Reset angles
 left_motor.reset_angle(0)
@@ -38,7 +40,7 @@ def move_motors(left_speed, right_speed, duration_ms=None, rotations=None, degre
 # ===== MAIN PROGRAM =====
 motor_a.run_angle(-1000, 280) # moves the white thing up
 
-move_motors(500, -500, duration_ms=1000) # aligns with the wall
+move_motors(500, -500, duration_ms=850) # aligns with the wall
 wait(500)
 
 move_motors(-300, 302, rotations=1.23) # moves to the first row of yellow blocks
@@ -54,42 +56,93 @@ wait(1000)
 
 motor_a.run_time(-1000, 900)  # move up the white thing
 
-move_motors(-100, 100, rotations=0.378) # moves to the second row of yellow blocks
+move_motors(-100, 100, rotations=0.385) # moves to the second row of yellow blocks
 left_motor.stop(Stop.COAST)
 right_motor.stop(Stop.COAST)
 
-motor_a.run_angle(200, 160) # moves the white thing down a little bit
+motor_a.run_angle(200, 120) # moves the white thing down a little bit
 wait(170)
 
 motor_d.stop()
 wait(50)
 
-motor_a.run_time(350, 500, wait=False) # moves the white thing down
-wait(500)
+#motor_a.run_time(200, 900) # moves the white thing down
 
-motor_d.run_time(-1000, 200) # releases the yellow block on top of the blue block
+motor_d.run_time(-300, 250) # releases the first row of yellow block on top of the second row 
 wait(250)
 
-motor_a.run_time(75, 1000, wait=False) # moves the white thing down
-'''
+motor_a.run_time(100, 1200, wait=False) # moves the white thing down
+wait(400)
+
 counter = 0
 while counter <= 3:
-    move_motors(-100, -100, degrees=20)
-    move_motors(100, 100, degrees=20)
+    move_motors(-100, -100, degrees=10)
+    move_motors(100, 100, degrees=10)
     counter +=1
 left_motor.stop(Stop.COAST)
 right_motor.stop(Stop.COAST)
-'''
+
+
 motor_d.run(500) # holds the block 
 wait(200)
 
 motor_a.run_angle(-1000, 200) # moves the white thing up
 
+left_motor.run_angle(-500,380) 
+
+move_motors(-500, 520, rotations=1.95) 
+wait(100)
+
+while csL.reflection() > 30:
+    left_motor.run(-500)
+    right_motor.run(500)
+
+left_motor.stop(Stop.BRAKE)
+right_motor.stop(Stop.BRAKE)
+wait(100)
+
+move_motors(-200, 200, rotations=0.1)
+wait(100)
+
+left_motor.run_angle(-400, 150)
+
+pid_line_follower(follow_sensor_port=Port.S1,
+                      stop_sensor_port=Port.S4,
+                      base_speed=250,
+                      Kp=2.5, Kd=3, Ki=0,
+                      target=50,
+                      max_angle=None,
+                      stop_mode="c",
+                      stop_threshold=20,
+                      side="l")
+
+
+wait(100)
+move_motors(-200, 200, rotations=0.85)
+
+motor_a.run_angle(1000, 200) # moves the white thing down to deposit on black thing
+wait(100)
+
+motor_d.stop()
+wait(50)
+
+motor_d.run_time(-300, 250) # releases the first row in the grid
+wait(100)
+
+motor_a.run_angle(-1000, 50) # mmoves the white thing up a little bit 
+wait(100)
+
+motor_d.run(500) # holds the blocks in place
+wait(100)
+
+move_motors(100, -100, degrees=360)
+wait(100)
+
+
+
+
+
 wait(10000)
-
-
-
-
 
 
 
@@ -106,4 +159,46 @@ while counter <= 3:
     counter +=1
 left_motor.stop(Stop.COAST)
 right_motor.stop(Stop.COAST)
+'''
+
+
+
+'''
+#putting the blocks in the black grid
+motor_d.run(500) # holds the blocks in place
+wait(1000)
+
+motor_a.run_angle(-1000, 280) # moves the white thing up
+
+move_motors(-300, 300, rotations=0.85)
+
+motor_a.run_angle(600, 250) # move down the white thing to take first row of yellow blocks
+
+motor_d.stop()
+wait(50)
+
+motor_d.run_time(-1000, 200) # releases the yellow block on top of the blue block
+
+motor_a.run_angle(-1000, 160) # moves the white thing up
+
+wait(2000)
+
+counter = 0
+while counter <= 3:
+    move_motors(-100, -100, degrees=20)
+    move_motors(100, 100, degrees=20)
+    wait(100)
+    counter +=1
+left_motor.stop(Stop.COAST)
+right_motor.stop(Stop.COAST)
+
+move_motors(-100, -100, degrees=20)
+move_motors(100, 100, degrees=30)
+
+wait(100)
+
+motor_a.run_angle(-1000, 110) # moves the white thing up
+
+
+wait(100000)
 '''
